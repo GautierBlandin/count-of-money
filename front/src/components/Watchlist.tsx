53c0e73cbd7c12962975ed53e2c89c8befc528cd
Gautier <gautier.blandin@gmail.com>
2021-12-19 18:55:15 +0100

    Add real-time crypto data to the watchlist component

--- front/src/components/Watchlist.tsx
+++ front/src/components/Watchlist.tsx
@@ -1,76 +1,55 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
 import{
-  useMemo,
-  useContext,
-  useEffect,
   useState
 } from "react";
-import {useLocation} from "react-router-dom";
-import axios from 'axios';
 
 import {
   Input, Form
 } from 'reactstrap';
 
 import './Watchlist.css'
+import {getCryptos} from "../api/Cryptos";
+import {AuthContext} from "../context/auth.context";
 
-type RowData = {
-  nameCryptoRow: string;
+interface watchListRow{
+  name: string,
+  symbol: string,
+  price: number,
+  changePercent: number,
+  highestDaily: number,
+  lowestDaily: number,
 }
 
-interface IState {
-  rows: RowData[];
-}
-
-// Use async if await axios
-// export default async function Watchlist() {
 export default function Watchlist() {
+  const authContext = useContext(AuthContext);
 
-  // axios
-  const [openingPrice, setOpeningPrice] = useState({})
-  const [currentPrice, setCurrentPrice] = useState({})
-
-  useEffect( () => {
-
-    /*
-    const res = await axios.get('http://localhost:3000/cryptos/btc');
-    setCoinres(res.data)
-    console.log('opening price', res.data.openingPrice)
-    */
-
-    axios.get('http://localhost:3000/cryptos/btc').then((res) => {
-      setOpeningPrice(res.data);
-      console.log('opening price', res.data.openingPrice)
-    })
-
-    axios.get('http://localhost:3000/cryptos/btc').then((res) => {
-      setCurrentPrice(res.data);
-      console.log('current price', res.data.currentPrice)
-    })
-
-  }, [])
-
-  // onClick: add cryptocurrency (row) to watchlist (table)
-  const [state, setState] = useState<IState>({rows: []});
-
-  const addRow = () => {
-    setState({
-      rows: [...state.rows, { nameCryptoRow: crypto }]
-    })
-  }
-
-  const { rows } = state;
-
-  // onChange: cryptocurrency choice (input)
+  const [watchlist, setWatchlist] = useState<string[]>([]);
+  const [watchlistRows, setWatchlistRows] = useState<watchListRow[]>([]);
   const [crypto, setCrypto] = useState("");
 
-  /*
-  // DEBUG
-  const handleSubmit = (evt: { preventDefault: () => void; }) => {
-    evt.preventDefault();
-    console.log({crypto})
-  }
-  */
+  const addCrypto = () => {
+    if(!watchlist.includes(crypto)){
+      setWatchlist([...watchlist, crypto]);
+      setCrypto("");
+    }
+  };
+
+  useEffect(() => {
+    console.log(watchlist)
+    getCryptos(watchlist).then(cryptos => {
+      cryptos.cryptos.map(crypto => {
+        setWatchlistRows([...watchlistRows, {
+          name: crypto.name,
+          symbol: crypto.symbol,
+          price: crypto.currentPrice,
+          changePercent: (crypto.currentPrice - crypto.openingPrice) / crypto.openingPrice  * 100,
+          highestDaily: crypto.highestDailyPrice,
+          lowestDaily: crypto.lowestDailyPrice
+        }])
+      })
+    });
+  }, [JSON.stringify(watchlist)])
+
 
   return (
 
@@ -91,12 +70,13 @@ export default function Watchlist() {
                   crypto="text"
                   placeholder="Enter crypto: BTC, ETH, ..."
                   type="text"
+                  value = {crypto}
                   onChange={e => setCrypto(e.target.value)}
                 />
               </Form>
             </div>
 
-            <button onClick={addRow} className="custom-button" role="button">Add to watchlist</button>
+            <button onClick={addCrypto} className="custom-button" role="button">Add to watchlist</button>
 
           </div>
 
@@ -104,22 +84,20 @@ export default function Watchlist() {
             <thead>
               <tr>
                 <th scope="col">Crypto</th>
-                <th scope="col">Rank</th>
                 <th scope="col">Price</th>
-                <th scope="col">24h Price Change</th>
-                <th scope="col">Market cap</th>
-                <th scope="col">24h Volume</th>
+                <th scope="col">Highest 24h</th>
+                <th scope="col">Lowest 24h</th>
+                <th scope="col">Change Percent</th>
               </tr>
             </thead>
             <tbody>
-                { rows.map(element => (
+                { watchlistRows.map(element => (
                   <tr>
-                    <td>{element.nameCryptoRow}</td> {/* const from onChange input */}
-                    <td><span className="rank">1</span></td>
-                    <td><span className="dollar">49759</span></td>
-                    <td><span className="percent">-3</span></td>
-                    <td><span className="dollar">879,039,487</span></td>
-                    <td><span className="dollar">106,846,038</span></td>
+                    <td>{element.name}</td> {/* const from onChange input */}
+                    <td><span className="dollar">{element.price}</span></td>
+                    <td><span className="dollar">{element.highestDaily}</span></td>
+                    <td><span className="dollar">{element.lowestDaily}</span></td>
+                    <td><span className="percent">{element.changePercent}</span></td>
                   </tr>
                 ))
               }
